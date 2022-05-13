ce5ba24c73474fdc62ff2b3bb398dea41ff49912
Aimbe <qkr2435@gmail.com>
2022-05-13 17:39:11 +0900

    Update index.js
    todo dynamic

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
       );
     }
   }
-  
+  // 기능 동적으로 구현하기
   class Board extends React.Component {
     renderSquare(i) {
       return <Square value={i} />;
